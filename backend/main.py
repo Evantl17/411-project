667025f1ecf06b7575a53345d9db6f3c470241bb
kwabenabu <kwabamp@bu.edu>
2024-04-05 11:50:03 -0400

    i added the topartist

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import time
 from flask import Flask, request, url_for, session, redirect
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 
 app = Flask(__name__)
 #creating a temp cookie to load faster
@@ -12,48 +13,64 @@ TOKEN_INFO = 'token_info'
 
 @app.route('/')
 def login():
-    #calling the authorization function 
+     #calling the authorization function 
     auth_url = create_spotify_oauth().get_authorize_url()
-    #this will retunr the url for it 
+     #this will retunr the url for it 
     return redirect(auth_url)
 
 @app.route('/redirect')
-def redirect_():
-    #create a new session
+def redirect_page():
+     #create a new session
     session.clear()
+    #requestinig the actual token under
     code = request.args.get('code')
     token_info = create_spotify_oauth().get_access_token(code)
     session[TOKEN_INFO] = token_info
-    return redirect(url_for('authenticate', _external=True))
+    return redirect(url_for('get_top_artists', _external=True))
 
-@app.route('/authenticate')
-def authenticate():
+@app.route('/get_top_artists')
+def get_top_artists():
     try:
+        #using token to get acces
         token_info = get_token()
-        return "OAUTH Successful"
+        #use spotipy spotify api to access the account
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+        #api documentation for top artist only grabbing the 4
+        results = sp.current_user_top_artists(limit=5)
+        #lc for getting the artist in the top artiist
+        top_artists = [artist['name'] for artist in results['items']]
+        #printing the array of top artist
+        return f"Your top artists: {top_artists}"
     except:
+        #if doesnt work redrect the user to the no login page
         print("User not logged in")
         return redirect('/')
 
 def get_token():
     token_info = session.get(TOKEN_INFO, None)
+    #if theres no token or 60 seconds past before token refresh ti and send it
     if not token_info:
-        return redirect(url_for('login', _external=False))
+        raise Exception("No token info")
     now = int(time.time())
     is_expired = token_info['expires_at'] - now < 60
     if is_expired:
         spotify_oauth = create_spotify_oauth()
         token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
+        session[TOKEN_INFO] = token_info
     return token_info
 
 def create_spotify_oauth():
     return SpotifyOAuth(
+        #the BS from the actual api dashboard info
         client_id='a8dd8b7decf845e1b7becdc8dbb909e3',
         client_secret="c6cfafd242384321b9c4aa81b58fb5fd",
-        redirect_uri=url_for('redirect_', _external=True),
+        redirect_uri=url_for('redirect_page', _external=True),
         scope='user-top-read'
     )
 
 if __name__ == '__main__':
-
-    app.run(debug=True, port=5000, host='128.197.29.253')
+    #fix the host and port issues so it works on other users computers
+    #deploy on the interentt
+    #do a gitpull a run locally
+    #implement database logic mongodb or sqL
+    app.run(debug=True, port=5000, host='0.0.0.0')
